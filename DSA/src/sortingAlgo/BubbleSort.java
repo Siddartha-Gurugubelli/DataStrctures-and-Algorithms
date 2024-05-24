efba68dfbb7b8976a7553e91d53482db4af9d1e6
Siddartha-Gurugubelli <siddarthagurugubelli@gmail.com>
2024-05-23 19:29:33 -0500

    bubble sort

--- DSA/src/sortingAlgo/BubbleSort.java
+++ DSA/src/sortingAlgo/BubbleSort.java
@@ -3,29 +3,30 @@ package sortingAlgo;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int [] arr = {10,6,2,8,12,1,5};
-		
-		int[] result = bubbleSort(arr);
-		for(int element:result) {
-			System.out.print(element+",");
-		}
 
+		int[] result = bubbleSort(arr);
+		for(int element: result) {
+			System.out.print(element + ",");
+		}
 	}
 
+	// BubbleSort
 	public static int[] bubbleSort(int[] arr) {
-		// TODO Auto-generated method stub
-		int temp=0;
-		for(int i=0;i<arr.length;i++) {
-			for(int j =0;j<arr.length-1-i;j++) {
-				if(arr[j]>arr[j+1]) {
-					temp = arr[j];
-					arr[j]= arr[j+1];
-					arr[j+1]=temp;
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length - 1 - i; j++) {
+				if(arr[j] > arr[j + 1]) {
+					swap(arr, j, j + 1);
 				}
 			}
 		}
 		return arr;
 	}
 
+	// Swap function
+	public static void swap(int[] arr, int index1, int index2) {
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
 }
